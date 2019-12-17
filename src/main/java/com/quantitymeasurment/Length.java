e661897f66c4c083e17e6297c78a57e23df3920a
PriyankaBhiogade <bhiogadePriyanka93@gmail.com>
2019-12-17 14:45:31 +0530

    Added: Test For Comapring two value

--- src/main/java/com/quantitymeasurment/Length.java
+++ src/main/java/com/quantitymeasurment/Length.java
@@ -4,7 +4,19 @@ import java.util.Objects;
 
 public class Length {
 
-    enum Unit {FEET, INCH;}
+    private static final double FEET_TO_INCH = 12.0 ;
+
+    public Boolean compare(Length that) {
+        if(this.unit.equals(that.unit) )
+        return this.equals(that);
+        if(this.unit.equals(Unit.FEET)  && that.unit.equals(Unit.INCH))
+        return Double.compare((this.value*FEET_TO_INCH),that.value) == 0;
+        if(this.unit.equals(Unit.FEET)  && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value,that.value) == 0;
+        return false;
+    }
+
+    enum Unit {FEET, INCH };
 
     private final Unit unit;
     private final double value;
--- src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
@@ -45,4 +45,27 @@ public class QuantityMeasurementTest {
         Length inch1 = new Length(Length.Unit.INCH,1.0);
         Assert.assertNotEquals(feet1,inch1);
     }
+
+    @Test
+    public void given0FeetAnd0Inch_ShouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Boolean compareCheck = feet1.compare(inch1);
+        Assert.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1FeetAnd1Inch_ShouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET,1.0);
+        Length inch1 = new Length(Length.Unit.INCH,1.0);
+        Boolean compareCheck = feet1.compare(inch1);
+        Assert.assertFalse(compareCheck);
+    }
+    @Test
+    public void given1FeetAnd1Feet_ShouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET,1.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
+        Boolean compareCheck = feet1.compare(feet2);
+        Assert.assertTrue(compareCheck);
+    }
 }
