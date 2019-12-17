b3dcb03984b00c26250fdee878c590ca2aff8f55
PriyankaBhiogade <bhiogadePriyanka93@gmail.com>
2019-12-17 17:11:56 +0530

    Added Test case:when values are 1 inch and 1 feet should return false value

--- src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
@@ -26,7 +26,7 @@ public class QuantityMeasurementTest {
     }
 
     @Test
-    public void givenValueZeroAndNullInFeet1_ShouldReturnWrongValue() {
+    public void givenTwoClassObject_WhenDifferent_ShouldReturnWrongValue() {
         Length feet1 = new Length(Length.Unit.FEET,0.0);
         Inch inch1 = new Inch(0.0);
         Assert.assertNotEquals(feet1,inch1);
@@ -68,11 +68,20 @@ public class QuantityMeasurementTest {
         Boolean compareCheck = feet1.compare(inch1);
         Assert.assertFalse(compareCheck);
     }
+
     @Test
     public void given1FeetAnd1Feet_ShouldReturnEqualLength() {
         Length feet1 = new Length(Length.Unit.FEET,1.0);
         Length feet2 = new Length(Length.Unit.FEET,1.0);
         Boolean compareCheck = feet1.compare(feet2);
         Assert.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1InchAnd1Feet_WhenNotEqual_ShouldReturnFalse() {
+        Length inch1 = new Length(Length.Unit.INCH,1.0);
+        Length feet1 = new Length(Length.Unit.FEET,1.0);
+        Boolean compareCheck = inch1.compare(feet1);
+        Assert.assertFalse(compareCheck);
     }
 }
