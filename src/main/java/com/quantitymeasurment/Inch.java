9fd3a02d1928b0f815380cd4c2612192e47ad044
PriyankaBhiogade <bhiogadePriyanka93@gmail.com>
2019-12-17 13:52:38 +0530

    Added: Code & Test For same value of inch check equal or not

---
+++ src/main/java/com/quantitymeasurment/Inch.java
@@ -0,0 +1,20 @@
+package com.quantitymeasurment;
+
+import java.util.Objects;
+
+public class Inch {
+    private final double value;
+
+    public Inch(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inch inch = (Inch) o;
+        return Double.compare(inch.value, value) == 0;
+    }
+
+}
--- src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
     @Test
-    public void givenValueInZeroFeetAndZeroFeet_ShouldReturnWrongValue() {
+    public void givenValueInZeroFeetAndZeroFeet_ShouldReturnEqualValue() {
         QuantityMeasurement feet1 = new QuantityMeasurement(0.0);
         QuantityMeasurement feet2 = new  QuantityMeasurement(0.0);
         Assert.assertEquals(feet1,feet2);
@@ -17,5 +17,12 @@ public class QuantityMeasurementTest {
         QuantityMeasurement feet1 = new QuantityMeasurement(0.0);
         QuantityMeasurement feet2 = new QuantityMeasurement(1.0);
         Assert.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenValueInZeroInchAndZeroInch_ShouldReturnEqualValue() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
     }
 }
