59251527dd0bb72455432d860cbc2468359842b5
PriyankaBhiogade <bhiogadePriyanka93@gmail.com>
2019-12-18 17:14:52 +0530

    Refactor : Code For Comparing Two units

--- src/main/java/com/quantitymeasurment/QuantityMeasurement.java
+++ src/main/java/com/quantitymeasurment/QuantityMeasurement.java
@@ -1,26 +1,26 @@
 package com.quantitymeasurment;
 
-public class Length {
+public class QuantityMeasurement {
 
-    public UnitValue unit;
+    public UnitValues unit;
     public  Double value;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Length length = (Length) o;
+        QuantityMeasurement length = (QuantityMeasurement) o;
         return Double.compare(length.value, value) == 0 &&
                 unit == length.unit;
     }
 
-    public Length(UnitValue unit, Double value) {
+    public QuantityMeasurement(UnitValues unit, Double value) {
         this.unit = unit;
         this.value = value;
     }
 
-    public boolean compare(Length that) {
-        Boolean conversion = UnitValue.conversion(that, this);
+    public boolean compare(QuantityMeasurement that) {
+        Boolean conversion = UnitValues.conversion(that, this);
         return conversion;
     }
 }
