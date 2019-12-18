b9f5c067213a0201de1a0034d35fb02fa38b2930
PriyankaBhiogade <bhiogadePriyanka93@gmail.com>
2019-12-18 18:57:31 +0530

    Added: Code For Performing Addition of Units

--- src/main/java/com/quantitymeasurment/UnitValues.java
+++ src/main/java/com/quantitymeasurment/UnitValues.java
@@ -1,7 +1,7 @@
 package com.quantitymeasurment;
 
 public enum UnitValues {
-    FEET(12.0), INCH(1.0), YARD(36.0);
+    FEET(12.0), INCH(1.0), YARD(36.0) ,CENTIMETER(0.4);
 
     public final double measurementValue;
 
@@ -11,5 +11,10 @@ public enum UnitValues {
 
     public static Boolean conversion(QuantityMeasurement val1, QuantityMeasurement val2) {
         return Double.compare((val1.value * val1.unit.measurementValue), (val2.value * val2.unit.measurementValue)) == 0;
+    }
+
+    public static Double addition(QuantityMeasurement val1, QuantityMeasurement val2) {
+        return ((val1.value * val1.unit.measurementValue) + (val2.value * val2.unit.measurementValue)) ;
+
     }
 }
--- src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurment/QuantityMeasurementTest.java
@@ -178,4 +178,44 @@ public class QuantityMeasurementTest {
         QuantityMeasurement yard2 = new QuantityMeasurement(UnitValues.INCH, 1.0);
         Assert.assertNotEquals(yard1,yard2);
     }
+
+    @Test
+    public void given2InchAnd5Centimeter_ShouldReturnTrueValue() {
+        QuantityMeasurement inch = new QuantityMeasurement(UnitValues.INCH, 2.0);
+        QuantityMeasurement cm = new QuantityMeasurement(UnitValues.CENTIMETER, 5.0);
+        boolean compareCheck = inch.compare(cm);
+        Assert.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given2InchAnd2Inch_ShouldReturn4Inches() {
+        QuantityMeasurement inch1 = new QuantityMeasurement(UnitValues.INCH, 2.0);
+        QuantityMeasurement  inch2 = new QuantityMeasurement(UnitValues.INCH, 2.0);
+        double result = inch1.additionOfLenghths(inch2);
+        Assert.assertEquals(4.0,result,0.0);
+    }
+
+    @Test
+    public void given1FeetAnd2Inch_ShouldReturn14Inches() {
+        QuantityMeasurement feet = new QuantityMeasurement(UnitValues.FEET, 1.0);
+        QuantityMeasurement  inch = new QuantityMeasurement(UnitValues.INCH, 2.0);
+        double result = feet.additionOfLenghths(inch);
+        Assert.assertEquals(14.0,result,0.0);
+    }
+
+    @Test
+    public void given1FeetAnd1Feet_ShouldReturn24Inches() {
+        QuantityMeasurement feet1 = new QuantityMeasurement(UnitValues.FEET, 1.0);
+        QuantityMeasurement  feet2 = new QuantityMeasurement(UnitValues.FEET, 1.0);
+        double result = feet1.additionOfLenghths(feet2);
+        Assert.assertEquals(24.0,result,0.0);
+    }
+
+    @Test
+    public void given2InchesAnd2_5Centimeters_ShouldReturn3Inches() {
+        QuantityMeasurement inch = new QuantityMeasurement(UnitValues.INCH, 2.0);
+        QuantityMeasurement centimeter = new QuantityMeasurement(UnitValues.CENTIMETER, 2.5);
+        double result = inch.additionOfLenghths(centimeter);
+        Assert.assertEquals(3.0, result, 0.0);
+    }
 }
