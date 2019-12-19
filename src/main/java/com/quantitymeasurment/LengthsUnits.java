ac5a3f87ce107cc9e684976bf0cd8148bae13d24
PriyankaBhiogade <bhiogadePriyanka93@gmail.com>
2019-12-19 10:07:05 +0530

    Refactor: code Rename Variable Name

--- src/main/java/com/quantitymeasurment/LengthsUnits.java
+++ src/main/java/com/quantitymeasurment/LengthsUnits.java
@@ -1,20 +1,19 @@
 package com.quantitymeasurment;
 
-public enum UnitValues {
+public enum LengthsUnits {
     FEET(12.0), INCH(1.0), YARD(36.0) ,CENTIMETER(0.4);
 
     public final double measurementValue;
 
-    UnitValues(Double measurementValue) {
+    LengthsUnits(Double measurementValue) {
         this.measurementValue = measurementValue;
     }
 
     public static Boolean conversion(QuantityMeasurement val1, QuantityMeasurement val2) {
-        return Double.compare((val1.value * val1.unit.measurementValue), (val2.value * val2.unit.measurementValue)) == 0;
+        return Double.compare((val1.value * val1.lengthsUnits.measurementValue), (val2.value * val2.lengthsUnits.measurementValue)) == 0;
     }
 
     public static Double addition(QuantityMeasurement val1, QuantityMeasurement val2) {
-        return ((val1.value * val1.unit.measurementValue) + (val2.value * val2.unit.measurementValue)) ;
-
+        return ((val1.value * val1.lengthsUnits.measurementValue) + (val2.value * val2.lengthsUnits.measurementValue)) ;
     }
 }
